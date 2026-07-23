167c6e18b6c93f727c29417a651bd9d24852eb90
CaduBD <cadubatistadiniz@gmail.com>
2026-07-23 15:58:00 -0300

    feat: configura pinos do LDR e botao, adiciona mensagem de inicializacao

--- src/main.py
+++ src/main.py
@@ -1,1 +1,12 @@
-print("Sistema Kanban Inicializado")
+from machine import Pin, ADC
+import time
+
+# Configuracao de hardware
+btn = Pin(13, Pin.IN, Pin.PULL_UP)
+ldr = ADC(Pin(32))
+
+ldr.atten(ADC.ATTN_11DB) #Permite ler toda a faixa de 0V a ~3.3V
+
+print("Contador de Producao Inicializado")
+
+
